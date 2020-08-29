6a911237feda970808d65687e01a205d0fc917b4
akshat <akshat_g@ch.iitr.ac.in>
2020-08-29 14:48:19 +0530

    Changed font style

--- src/Header.js
+++ src/Header.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import "./Header.css"
+import {Avatar} from "@material-ui/core"
+import NotificationsActiveIcon from '@material-ui/icons/NotificationsActive';
+import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import ChatBubbleIcon from '@material-ui/icons/ChatBubble';
 import SearchIcon from '@material-ui/icons/Search';
 import HomeIcon from '@material-ui/icons/Home';
 import AssistantPhotoIcon from '@material-ui/icons/AssistantPhoto';
@@ -12,7 +16,7 @@ function Header() {
              <img src="https://upload.wikimedia.org/wikipedia/commons/5/51/Facebook_f_logo_%282019%29.svg"/>
              <div className="header__search">
                  <SearchIcon/>
-                 <input type="text" placeholder="Search"></input>
+                 <input type="text" placeholder="Search Anything"></input>
              </div>
             </div>
             <div className="header__center">
@@ -30,7 +34,17 @@ function Header() {
              </div>
             </div>
             <div className="header__left">
-
+             <Avatar/>
+             <h4>UserName</h4>
+             <div className="left__options">
+             <ChatBubbleIcon fontSize="small"/>
+             </div>
+             <div className="left__options">
+             <NotificationsActiveIcon fontSize="small"/>
+             </div>
+             <div className="left__options">
+             <KeyboardArrowDownIcon fontSize="small"/>
+             </div>
             </div>
             
         </div>
